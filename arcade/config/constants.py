9659e84f647f0542c0a8d8060359b8b5d54b901f
nerdimite <bhavesh.laddagiri1@gmail.com>
2025-04-13 09:30:06 +0000

    Add new dependencies for testing and code coverage, including pytest, coverage, and pytest-mock. Update project configuration for testing and add pytest.ini for test discovery and settings.

--- arcade/config/constants.py
+++ arcade/config/constants.py
@@ -4,9 +4,9 @@ PROMPTS_PATH = Path(__file__).parent.parent / "prompts"
 
 # DynamoDB Tables
 TEAMS_TABLE = "logic-arcade-teams"
-
 PP_IMAGES_TABLE = "pic-perfect-images"
 PP_LEADERBOARD_TABLE = "pic-perfect-leaderboard"
+ARCADE_STATE_TABLE = "arcade-challenge-state"
 
 # Maximum number of votes a team can cast
 MAX_VOTES_PER_TEAM = 3
